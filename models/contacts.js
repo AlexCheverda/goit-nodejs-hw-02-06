56722b2de04c0bd55c78503353e79c38b9638b99
Aleksandr Cheverda <alexar@ua.fm>
2023-02-11 11:29:36 +0200

    upd files

--- models/contacts.js
+++ models/contacts.js
@@ -1,20 +1,22 @@
-const fs = require("fs/promises");
+const fs = require("fs").promises;
 const path = require("path");
-const { nanoid } = require("nanoid");
+const { v4 } = require("uuid");
+
 const contactsPath = path.join(__dirname, "contacts.json");
 
-const updateContacts = async (contacts) =>
+const updateContacts = async (contacts) => {
   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
 
 const listContacts = async () => {
-  const data = await fs.readFile(contactsPath);
+  const data = await fs.readFile(contactsPath, "utf-8");
   return JSON.parse(data);
 };
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
   const idToString = String(contactId);
-  const contactById = contacts.find((item) => item.id === idToString);
+  const contactById = contacts.find((item) => item.id === contactId);
   return contactById || null;
 };
 
@@ -31,21 +33,24 @@ const removeContact = async (contactId) => {
 
 const addContact = async ({ name, email, phone }) => {
   const contacts = await listContacts();
-  const newContact = { id: nanoid(), name, email, phone };
+  const newContact = { id: v4(), name, email, phone };
   contacts.push(newContact);
   await updateContacts(contacts);
   return newContact;
 };
 
-const updateContact = async (contactId, { name, email, phone }) => {
+const updateContact = async (id, data) => {
   const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  contacts[index] = { contactId, name, email, phone };
-  await updateContacts(contacts);
-  return contacts[index];
+  let newContact;
+  const updatedContacts = contacts.map((contact) => {
+    if (contact.id === id) {
+      newContact = { ...contact, ...data };
+      return newContact;
+    }
+    return contact;
+  });
+  await updateContacts(updatedContacts);
+  return newContact;
 };
 
 module.exports = {
