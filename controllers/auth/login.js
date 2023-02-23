3c15d1f77f00a4bf1086de99baa1e65665e379bc
Aleksandr Cheverda <alexar@ua.fm>
2023-02-23 21:58:40 +0200

    upd files

---
+++ controllers/auth/login.js
@@ -0,0 +1,14 @@
+const { Unauthorized } = require("http-errors");
+const bcrypt = require("bcryptjs");
+const { User } = require("../../models");
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw new Unauthorized();
+    };
+    const passCompare = bcrypt.compareSync();
+};
+
+module.exports = login;
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { controllerWrapper, validatIdParam } = require('../../helpers'); 
 const { auth: ctrl } = require("../../controllers");
+const { joiRegisterSchema, joiLoginSchema } = require("../../models/user");
 
 const router = express.Router();
 
-router.post('/register', controllerWrapper(ctrl.register));
+router.post('/register', validatIdParam(joiRegisterSchema), controllerWrapper(ctrl.register));
 // router.post('/signup');
+
+router.post('/login', validatIdParam(joiLoginSchema), controllerWrapper(ctrl.login));
+// router.post("/signin");
 
 module.exports = router;
