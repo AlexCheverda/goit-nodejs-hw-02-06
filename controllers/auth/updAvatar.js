ca808f30b64e411208c323506e1b959d57eb69c2
Aleksandr Cheverda <alexar@ua.fm>
2023-02-26 21:57:16 +0200

    refact files

--- controllers/auth/updAvatar.js
+++ controllers/auth/updAvatar.js
@@ -1,18 +1,27 @@
 const { User } = require("../../models");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const updAvatar = async (req, res) => {
     const { path: tempUpload, originalname } = req.file;
     const { _id: id } = req.user;
-    const imageName = `${id}_${originalname}`;
+    const extension = originalname.split(".").pop();
+    const imageName = `${id}_${extension}`;
     const resultUpload = path.join(avatarsDir, imageName);
     try {   
         await fs.rename(tempUpload, resultUpload);
-        const avatarURL = path.join("public", "avatars", imageName);
-        await User.findByIdAndUpdate(req.user._id, { avatarURL });
+        Jimp.read(resultUpload).then((imageAvatar) => {
+            return imageAvatar.resize(256, 256).write(resultUpload);
+        })
+            .catch((err) => {
+                console.error(err);
+            });
+        const HOST = `http://localhost:${process.env.PORT}`;
+        const avatarURL = `${HOST}/avatars/${imageName}`;
+        await User.findByIdAndUpdate(_id, { avatarURL });
         res.json({ avatarURL });
     } catch (error) {
         await fs.unlink(tempUpload);
